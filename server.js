3795147bff5f42b50a72e342d269215903c14c8c
zertyg1 <abdoxvito@gmail.com>
2024-01-03 00:54:00 +0100

    server updates

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express=request('express');
 const path=require('path');
 const app=express();
-app.use(express.static(__dirname+'/dist/Angular-Miage'));
+app.use(express.static(__dirname+'/dist/assignment-app/'));
 app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/Angular-Miage/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/assignment-app/index.html'));
 }); 
 app.listen(process.env.PORT || 8081);
