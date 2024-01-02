238974aadec0c7fd0ccb557c66345dc7aa83d1ed
zertyg1 <abdoxvito@gmail.com>
2024-01-03 00:59:00 +0100

    update server

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-const express=request('express');
-const path=require('path');
-const app=express();
-app.use(express.static(__dirname+'/dist/assignment-app/'));
-app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/assignment-app/index.html'));
-}); 
+//Install express server
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + "/dist/assignment-app/"));
+
+app.get("/*", function (req, res) {
+ res.sendFile(path.join(__dirname + "/dist/assignment-app/index.html"));
+});
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8081);
